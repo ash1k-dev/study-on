5dd468c6c13b023abc70bd4d1290542b794960ed
ash1k-dev <ash1kekb@yandex.ru>
2024-02-04 21:33:17 +0500

    feat: add api-auth in config

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     # User management
     path("users/", include("study_on.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
+    path("api-auth/", include("rest_framework.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
