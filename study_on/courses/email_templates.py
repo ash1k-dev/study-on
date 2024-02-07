f5d5fb22473ad6793f0e37737b2fdcd6beb990f2
ash1k-dev <ash1kekb@yandex.ru>
2024-02-07 19:26:30 +0500

    feat: add email templates and change course reminder func

---
+++ study_on/courses/email_templates.py
@@ -0,0 +1,10 @@
+from string import Template
+
+TEXT_EMAIL_REMINDER = Template(
+    "<b>Здравствуйте, ${student}</b>\n"
+    "Вы давно не посещали курс ${course_name}\n"
+    "Не потеряйте свой прогресс, успех в обучении всегода приходит при регулярных занятиях\n"
+    "Возвращайтесь, мы Вас ждем!"
+)
+
+TEXT_EMAIL_GREETING = Template("Здравствуйте, ${student}. Ваш курс ждет Вас!\n")
--- study_on/courses/tasks.py
+++ study_on/courses/tasks.py
@@ -1,32 +1,14 @@
-import smtplib
 from datetime import datetime, timedelta
-from email.message import EmailMessage
+from os import getenv
 
+from django.core import mail
 from django.db.models import Q
 
 from config import celery_app
+from study_on.courses.email_templates import TEXT_EMAIL_GREETING, TEXT_EMAIL_REMINDER
 from study_on.courses.models import AvailableLessons
 
-SMTP_HOST = "smtp.gmail.com"
-SMTP_PORT = 465
-SMTP_EMAIL = "your_email"
-SMTP_PASSWORD = "your_password"
-
-
-def get_email_user_template(student, student_email, course_name):
-    email = EmailMessage()
-    email["Subject"] = "Ваш курс ждет Вас!"
-    email["From"] = "StudyOn <" + SMTP_EMAIL + ">"
-    email["To"] = student_email
-
-    email.set_content(
-        "<div>"
-        f'<h1>Здравствуйте, {student}, Вы давно не посещали курс "{course_name}"</h1>'
-        "Возвращайтесь, мы Вас ждем!"
-        "</div>",
-        subtype="html",
-    )
-    return email
+STUDY_ON_EMAIL = getenv("STUDY_ON_EMAIL")
 
 
 @celery_app.task()
@@ -39,11 +21,17 @@ def course_reminder():
         )
         .filter(Q(updated__lt=datetime.now() - timedelta(days=3)))
     )
-    for result in uinited_results:
-        student = result.student.username
-        student_email = result.student.email
-        course_name = result.course.title
-        email = get_email_user_template(student, student_email, course_name)
-        with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
-            server.login(SMTP_EMAIL, SMTP_PASSWORD)
-            server.send_message(email)
+    with mail.get_connection() as connection:
+        for result in uinited_results:
+            student = result.student.username
+            student_email = result.student.email
+            course_name = result.course.title
+            subject = TEXT_EMAIL_GREETING.substitute(student=student)
+            body = TEXT_EMAIL_REMINDER.substitute(student=student, course_name=course_name)
+            mail.EmailMessage(
+                subject=subject,
+                body=body,
+                from_email="StudyOn <" + STUDY_ON_EMAIL + ">",
+                to=[student_email],
+                connection=connection,
+            ).send()
