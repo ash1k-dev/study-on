505d52146de9a6e82821b4e3e4a60300deacc938
ash1k-dev <ash1kekb@yandex.ru>
2024-06-12 01:04:01 +0500

    feat: post models

---
+++ study_on/blog/models/post.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from study_on.services.models import BaseModel
+
+
+class Post(BaseModel):
+    """Модель поста"""
+
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE, verbose_name=_("Автор"))
+    title = models.CharField(max_length=200, verbose_name=_("Заголовок"))
+    content = models.TextField(verbose_name=_("Содержание"))
+    slug = models.SlugField(max_length=200, unique=True, verbose_name=_("Ссылка"))
+    image = models.ImageField(upload_to="posts", null=True, blank=True, verbose_name=_("Изображение"))
+    is_published = models.BooleanField(default=False, verbose_name=_("Опубликовано"))
+
+    class Meta:
+        verbose_name = _("Пост")
+        verbose_name_plural = _("Посты")
+
+    def __str__(self):
+        return self.title
