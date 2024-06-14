b672c3736878f1e1540017b06b45e97ab779ff5a
ash1k-dev <ash1kekb@yandex.ru>
2024-06-15 04:28:40 +0500

    feat: add new logic for sending email

--- study_on/users/tasks.py
+++ study_on/users/tasks.py
@@ -10,6 +10,8 @@ from study_on.users.templates import (
     TEXT_IDENTIFICATION,
     TEXT_REPEATED_IDENTIFICATION_ERROR,
     TEXT_REWARD,
+    TEXT_SURVEY_APPROVE,
+    TEXT_SURVEY_DONE,
 )
 
 User = get_user_model()
@@ -25,18 +27,24 @@ def get_users_count():
 
 
 @celery_app.task()
-def send_email(username, email, email_type, reward=None, identification_code=None):
+def send_email(username, email, email_type, lesson=None, reward=None, identification_code=None):
     with mail.get_connection() as connection:
         email_type_dict = {
             "confirm": TEXT_IDENTIFICATION,
             "confirm_error": TEXT_REPEATED_IDENTIFICATION_ERROR,
             "change_password": TEXT_CHANGE_PASSWORD,
             "reward": TEXT_REWARD,
+            "survey_approve": TEXT_SURVEY_APPROVE,
+            "survey_done": TEXT_SURVEY_DONE,
         }
         email_text = email_type_dict.get(email_type)
         subject = TEXT_GREETING.substitute(user=username)
         if email_type == "reward":
             body = email_text.substitute(user=username, reward=reward)
+        elif email_type == "survey_approve":
+            body = email_text.substitute(user=username, lesson=lesson)
+        elif email_type == "survey_done":
+            body = email_text.substitute(user=username, lesson=lesson)
         else:
             body = email_text.substitute(user=username, code=identification_code)
         mail.EmailMessage(
